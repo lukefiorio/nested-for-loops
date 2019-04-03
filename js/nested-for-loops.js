6a0b2afb11dd0e9f809a57031c43a924260c2c72
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-02 17:01:50 -1000

    applied auto format

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -4,14 +4,14 @@
  * @param {number} [width=depth] 
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
+function nestedForLoops(depth, width = depth) {
   let result = "\n";
 
   // Write code here
-  for (let i=0;i<depth;i++) {
-    for (let j=0;j<width;j++) {
-      result += "{x:"+j+", y:"+i+"}";
-      if (j < width-1) result += ", ";
+  for (let i = 0; i < depth; i++) {
+    for (let j = 0; j < width; j++) {
+      result += "{x:" + j + ", y:" + i + "}";
+      if (j < width - 1) result += ", ";
     }
     result += "\n";
   }
